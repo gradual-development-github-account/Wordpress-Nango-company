95ddeb52430249563bae77988ff501d547ff5365
Alexandr Ja <mr-spinn@outlook.com>
2017-04-22 20:18:19 +0600

    build/../theme/sources/theme/scripts   JS:     add scripts for fullscreen slider   PAGES: sections

---
+++ build/wp-content/themes/nango-company/sources/theme/scripts/scripts-swiper.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.page-sections__swiper-slider-fullscreen-1', {
+	pagination: '.swiper-slider-fullscreen-1__swiper-pagination-1',
+	direction: 'vertical',
+	slidesPerView: 1,
+	paginationClickable: true,
+	mousewheelControl: true
+});
