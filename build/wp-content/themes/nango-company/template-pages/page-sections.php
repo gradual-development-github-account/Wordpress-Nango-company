ae9ce41358e8aa87ec18ab15447eb136ea215fca
Alexandr Ja <mr-spinn@outlook.com>
2017-04-22 20:20:46 +0600

    build/../theme/template-parts   PHP:     add  for fullscreen slider markup   PAGES: sections

--- build/wp-content/themes/nango-company/template-pages/page-sections.php
+++ build/wp-content/themes/nango-company/template-pages/page-sections.php
@@ -8,33 +8,24 @@ Template Name: Sections page
 <?php get_template_part('template-parts/head-page'); ?>
 
 
-<div class="">
-<div class="">
-  <?php get_template_part('template-parts/headers/header-1'); ?>
-
-  <main class="">
-
-    <div class="vendor  components  flexboxgrid  container-1170
-                modificators  container--center  container--full-height">
-      <h1>Sections page</h1>
-
-      <a href="#">Link</a>
-
-      <ul>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-
-
-  </main>
-</div>
-
-<div class="">
-<?php get_template_part('template-parts/footers/footer-1'); ?>
-</div>
+<div class="vendor  components    swiper-slider  swiper-container
+            vendor  modificators  swiper-slider  slider--fullscreen
+            page-sections__swiper-slider-fullscreen-1">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    <div class="swiper-slide">Slide 4</div>
+    <div class="swiper-slide">Slide 5</div>
+    <div class="swiper-slide">Slide 6</div>
+    <div class="swiper-slide">Slide 7</div>
+    <div class="swiper-slide">Slide 8</div>
+    <div class="swiper-slide">Slide 9</div>
+    <div class="swiper-slide">Slide 10</div>
+  </div>
+  <!-- Add Pagination -->
+  <div class="vendor  components  swiper-pagination
+              swiper-slider-fullscreen-1__swiper-pagination-1"></div>
 </div>
 
 
