0eef8206eca12c61cf2b6a7a32ddad917aa1ab1a
Alexandr Ja <mr-spinn@outlook.com>
2017-04-22 23:28:09 +0600

    build/../theme/template-parts   PHP:     add markup .col + .container + .col   PARTS: header-1

--- build/wp-content/themes/nango-company/template-parts/headers/header-1.php
+++ build/wp-content/themes/nango-company/template-parts/headers/header-1.php
@@ -1,3 +1,28 @@
-<header class="vendor  components  flexboxgrid  container-1720">
-  <h2>Header-1</h2>
+<header class="theme  template-parts  headers  header-1">
+
+  <div class="vendor components flexboxgrid  container-fluid">
+    <div class="vendor components flexboxgrid  row
+                vendor  modificators  flexboxgrid  container--justify-content-space-between">
+      <div class="vendor  components  flexboxgrid  container-1170">
+        <div class="vendor components flexboxgrid  row">
+          <div class="vendor  components  flexboxgrid  cols  col-xs-12">
+            <h2>Header-1</h2>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="vendor  components  flexboxgrid  cols  col-lg-1
+                  vendor  modificators  flexboxgrid  item--order--1">
+        <a href="#">Link Left</a>
+      </div>
+
+
+      <div class="vendor  components  flexboxgrid  cols  col-lg-1">
+        <a href="#">Link Right</a>
+      </div>
+    </div>
+  </div>
+
+
 </header>
