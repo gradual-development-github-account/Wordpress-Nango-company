6b49b65b8932cebb424753ca6417f73385f23c63
Alexandr Ja <mr-spinn@outlook.com>
2017-04-23 15:54:53 +0600

    build/../theme/template-parts   PHP:     update markup: add containers for header and page   PAGES: sections

--- build/wp-content/themes/nango-company/template-pages/page-sections.php
+++ build/wp-content/themes/nango-company/template-pages/page-sections.php
@@ -8,24 +8,39 @@ Template Name: Sections page
 <?php get_template_part('template-parts/head-page'); ?>
 
 
-<div class="vendor  components    swiper-slider  swiper-container
-            vendor  modificators  swiper-slider  slider--fullscreen
-            page-sections__swiper-slider-fullscreen-1">
-  <div class="swiper-wrapper">
-    <div class="swiper-slide">Slide 1</div>
-    <div class="swiper-slide">Slide 2</div>
-    <div class="swiper-slide">Slide 3</div>
-    <div class="swiper-slide">Slide 4</div>
-    <div class="swiper-slide">Slide 5</div>
-    <div class="swiper-slide">Slide 6</div>
-    <div class="swiper-slide">Slide 7</div>
-    <div class="swiper-slide">Slide 8</div>
-    <div class="swiper-slide">Slide 9</div>
-    <div class="swiper-slide">Slide 10</div>
+<div class="theme  components  template-parts  page-sections
+            theme  components  containers  relative-container
+            theme  modificators  containers  fullscreen">
+  
+  <div class="theme  components  template-parts  page-sections__header-wrapper
+              theme  components  containers  absolute-container
+              theme  modificators  containers  full-width">
+    <?php get_template_part('template-parts/headers/header-1'); ?>
   </div>
-  <!-- Add Pagination -->
-  <div class="vendor  components  swiper-pagination
-              swiper-slider-fullscreen-1__swiper-pagination-1"></div>
+  
+  
+  <div class="theme  components  template-parts  page-sections__slider-wrapper
+              vendor  components    swiper-slider  swiper-container
+              vendor  modificators  swiper-slider  slider--fullscreen
+              js-swiper-slider-fullscreen-1">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+      <div class="swiper-slide">Slide 10</div>
+    </div>
+    <!-- Add Pagination -->
+    <!--Pagination может не являться зависимым от компонента Slider -->
+    <div class="vendor  components  swiper-pagination
+                js-swiper-slider-fullscreen-1__pagination-1"></div>
+  </div>
+
 </div>
 
 
