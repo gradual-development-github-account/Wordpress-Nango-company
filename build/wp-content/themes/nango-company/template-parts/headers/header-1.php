5f10e027d8a11b03e787ad11b70a48dad94b0625
Alexandr Ja <mr-spinn@outlook.com>
2017-04-23 15:55:23 +0600

    build/../theme/template-parts   PHP:     add layers for header-1   PARTS: header-1

--- build/wp-content/themes/nango-company/template-parts/headers/header-1.php
+++ build/wp-content/themes/nango-company/template-parts/headers/header-1.php
@@ -1,28 +1,28 @@
-<header class="theme  template-parts  headers  header-1">
+<header class="theme  template-parts  headers  header-1
+               theme  components    containers  relative-container
+               theme  modificators  containers  content-box">
 
-  <div class="vendor components flexboxgrid  container-fluid">
-    <div class="vendor components flexboxgrid  row
-                vendor  modificators  flexboxgrid  container--justify-content-space-between">
-      <div class="vendor  components  flexboxgrid  container-1170">
-        <div class="vendor components flexboxgrid  row">
-          <div class="vendor  components  flexboxgrid  cols  col-xs-12">
-            <h2>Header-1</h2>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="vendor  components  flexboxgrid  cols  col-lg-1
-                  vendor  modificators  flexboxgrid  item--order--1">
-        <a href="#">Link Left</a>
-      </div>
-
-
-      <div class="vendor  components  flexboxgrid  cols  col-lg-1">
-        <a href="#">Link Right</a>
-      </div>
-    </div>
+  <div class="header-1__layer-1
+              vendor  components  flexboxgrid  container-1200
+              vendor  modificators  flexboxgrid  container--center">
+<!--    <h2>Header</h2>-->
   </div>
 
+  <div class="header-1__layer-2
+              theme  components  containers  absolute-container
+              theme  modificators  containers  full-width">
+    <div class="vendor  components  flexboxgrid  container-fluid
+                vendor  modificators  flexboxgrid  flex-container  container--justify-content-space-between">
+       <div>
+         <a class="theme  components  containers  block-container" href="#">
+           <?php get_template_part('template-parts/logos/logo-1'); ?>
+         </a>
+       </div>
+      <div>
+        <?php get_template_part('template-parts/navs/main-nav-1'); ?>
+      </div>
+    </div>
+
+  </div>
 
 </header>
