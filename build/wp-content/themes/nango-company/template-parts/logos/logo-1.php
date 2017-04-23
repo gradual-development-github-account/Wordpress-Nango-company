4ad7268c846a816d97ade5ebddb6c241e7a02119
Alexandr Ja <mr-spinn@outlook.com>
2017-04-23 16:06:12 +0600

    build/../theme/../logos   PHP:     init logo-1

---
+++ build/wp-content/themes/nango-company/template-parts/logos/logo-1.php
@@ -0,0 +1,14 @@
+<div class="theme  components  template-parts  logos  logo-1
+            theme  modificators  behaviors  animation  logos
+            vendor  modificators  flexboxgrid  flex-container  container--align-items-center">
+  <svg  class=""  width="100"  height="100"  viewBox="0 0 172.73 190.58">
+    <g id="Objects">
+      <polygon class="cls-1" points="90.26 61.24 143.18 30.68 112.88 13.19 90.48 0 90.26 0.13 74.98 8.95 90.26 17.95 105.03 26.66 112.02 30.68 90.26 43.24 90.26 61.24"/>
+      <polygon class="cls-2" points="172.73 99.85 172.73 38.74 90.26 86.36 90.26 104.35 157.15 65.73 157.15 90.86 90.26 129.47 90.26 147.46 172.73 99.85"/>
+      <polygon class="cls-3" points="90.26 172.58 90.26 190.58 131.5 166.77 150.22 155.96 172.73 142.74 172.73 124.66 142.37 142.5 90.26 172.58"/>
+      <polygon class="cls-4" points="90.26 172.58 23.38 133.97 23.38 108.85 90.26 147.46 90.26 147.46 90.26 129.47 90.26 129.47 7.79 81.86 7.79 142.97 90.26 190.58 90.26 190.58 90.26 172.58 90.26 172.58"/>
+      <polygon class="cls-5" points="31.17 52.24 52.92 39.68 90.26 61.24 90.26 43.24 52.92 21.68 30.36 34.71 0 52.24 90.26 104.35 90.26 104.35 90.26 86.36 31.17 52.24"/>
+    </g>
+  </svg>
+  <span class="theme  modificators  fonts  font-family-4  fonts-titles--md">Company</span>
+</div>
