0c1051a15871333af6e91e17de758a9a1363a97e
Alexandr Ja <mr-spinn@outlook.com>
2017-04-25 20:14:22 +0600

    init:   PHP:     build/../theme/template-*/       footer-2

---
+++ build/wp-content/themes/nango-company/template-parts/footers/footer-2.php
@@ -0,0 +1,72 @@
+<footer class="theme  template-parts  footers  footer-2">
+
+  <!--Component mask-->
+  <div class="theme  components  masks">
+    <div class="mask__content  footer-2__content">
+
+      <div class="theme  modificators  grid  container-fluid--plr-60">
+        <div class="vendor  components  flexboxgrid  container-fluid">
+          <div class="vendor  components  flexboxgrid  row">
+
+            <div class="vendor  components  flexboxgrid  col  col-sm-3">
+              <div class="footer-2__logo">
+                <a class="theme  components  containers  block-container" href="#">
+                  <?php get_template_part('template-parts/logos/logo-1'); ?>
+                </a>
+              </div>
+              
+              <p class="theme  modificators  fonts  fonts-paragraph--justify">Cras justo odio, dapibus ac facilrrisis in, egestas eget quam. Cumagnis dis parturient montes,</p>
+
+              <div class="footer-2__copyright">
+                <?php get_template_part('template-widgets/widgets-copyright/widget-copyright-1'); ?>
+              </div>
+            </div>
+
+
+            <div class="vendor  components  flexboxgrid  col  col-sm-2  col-sm-offset-1">
+              <?php get_template_part('template-widgets/widgets-categories-list/widget-categories-list-1'); ?>
+            </div>
+
+
+            <div class="vendor  components  flexboxgrid  col  col-sm-2  col-sm-offset-1">
+              <?php get_template_part('template-widgets/widgets-pages-list/widget-pages-list-1'); ?>
+            </div>
+
+
+            <div class="vendor  components  flexboxgrid  col  col-sm-3  col-sm-offset-1">
+              <?php get_template_part('template-widgets/widgets-social-icons/widget-social-icons-1'); ?>
+            </div>
+
+
+            <div class="vendor  components  flexboxgrid  col  col-sm-5">
+              <?php get_template_part('template-widgets/widgets-instagramm/widget-instagramm-1'); ?>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="mask__mask
+                  vendor  components  flexboxgrid  row">
+      <div class="vendor  components  flexboxgrid  col  col-xs-18
+                    theme  modificators  masks  mask-1"></div>
+    </div>
+
+
+    <div class="mask__image
+                  vendor  components  flexboxgrid  row">
+      <div class="theme  components  images  image-wrapper">
+        <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/bg/bg-11-b&w.jpg'; ?>" alt="alt">
+      </div>
+    </div>
+
+  </div>
+  <!--Component mask-->
+
+</footer>
+
+
+
