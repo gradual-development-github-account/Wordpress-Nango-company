82cb4671815664e45989b26dbaffdc6306d69448
Alexandr Ja <mr-spinn@outlook.com>
2017-04-28 09:04:00 +0600

    build/../theme/template-parts/logos/   PHP:     add svg in logo-1

--- build/wp-content/themes/nango-company/template-parts/logos/logo-1.php
+++ build/wp-content/themes/nango-company/template-parts/logos/logo-1.php
@@ -1,8 +1,12 @@
 <div class="theme  components  template-parts  logos  logo-1
             theme  modificators  behaviors  animation  logos
-            vendor  modificators  flexboxgrid  flex-container  container--align-items-center">
-  <svg  class=""  width="100"  height="100"  viewBox="0 0 172.73 190.58">
-    <g id="Objects">
+            vendor  modificators  flexboxgrid  flex-container  container--align-items-center  container--wrap">
+  <svg  class="theme  modificators  gradients  svg-gradient-1"  width="100"  height="100"  viewBox="0 0 172.73 190.58">
+    <linearGradient class="svg-gradient">
+      <stop class="svg-gradient--start" offset="0%" stop-color=""/>
+      <stop class="svg-gradient--end"   offset="100%" stop-color=""/>
+    </linearGradient>
+    <g id="Objects" class="svg-path" fill="#000000">
       <polygon class="cls-1" points="90.26 61.24 143.18 30.68 112.88 13.19 90.48 0 90.26 0.13 74.98 8.95 90.26 17.95 105.03 26.66 112.02 30.68 90.26 43.24 90.26 61.24"/>
       <polygon class="cls-2" points="172.73 99.85 172.73 38.74 90.26 86.36 90.26 104.35 157.15 65.73 157.15 90.86 90.26 129.47 90.26 147.46 172.73 99.85"/>
       <polygon class="cls-3" points="90.26 172.58 90.26 190.58 131.5 166.77 150.22 155.96 172.73 142.74 172.73 124.66 142.37 142.5 90.26 172.58"/>
@@ -10,5 +14,31 @@
       <polygon class="cls-5" points="31.17 52.24 52.92 39.68 90.26 61.24 90.26 43.24 52.92 21.68 30.36 34.71 0 52.24 90.26 104.35 90.26 104.35 90.26 86.36 31.17 52.24"/>
     </g>
   </svg>
-  <span class="theme  modificators  fonts  font-family-4  fonts-titles--md">Company</span>
+
+
+  <svg  class="theme  modificators  gradients  svg-gradient-1"  width="200" height="50" >
+    <defs>
+      <linearGradient class="svg-gradient"
+                      id="liGr-156xv1c"
+                      x1="0" y1="0" x2="100%" y2="0%">
+        <stop class="svg-gradient--start" offset="0%" stop-color=""/>
+        <stop class="svg-gradient--end"   offset="100%" stop-color=""/>
+      </linearGradient>
+
+      <pattern id="p-gradient"
+               patternUnits="userSpaceOnUse"
+               width="200" height="50"
+               viewbox="0 0 200 50">
+        <rect class="svg-path"
+              fill="url(#liGr-156xv1c)"
+              width="200"  height="50"/>
+      </pattern>
+    </defs>
+
+    <text
+        class="theme  modificators  fonts  font-family-4  fonts-titles--md"
+        x="0%" y="100%"
+        fill="url(#p-gradient)">Company</text>
+  </svg>
 </div>
+
