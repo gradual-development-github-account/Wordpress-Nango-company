543a227d8dd737ae8b309e84bbc3806e7f9962cf
Alexandr Ja <mr-spinn@outlook.com>
2017-05-02 21:42:07 +0600

    build/../theme/template-parts/   PHP:     init markup for header-2

---
+++ build/wp-content/themes/nango-company/template-parts/headers/header-2.php
@@ -0,0 +1,80 @@
+<header class="theme  template-parts  headers  header-2
+               theme  components  containers  relative-container">
+
+    <!--Component mask-->
+    <div class="theme  components  masks">
+      <div class="mask__content  header-2__content">
+
+        <div class="vendor  components  flexboxgrid  container-1800
+                    vendor  modificators  flexboxgrid  container--center">
+          <div class="vendor  components  flexboxgrid  row
+                      vendor  modificators  flexboxgrid  responsive  container--justify-content-center-lg">
+
+            <div class="vendor  components  flexboxgrid  col  col-xs-10  col-sm-6  col-md-6
+                        vendor  components  flexboxgrid  container-1200--half-lg
+                        vendor  modificators  flexboxgrid  responsive  container-1200--half-lg">
+              <div class="header-2__logo">
+                <a class="theme  components  containers  block-container" href="#">
+                  <?php get_template_part('template-parts/logos/logo-1'); ?>
+                </a>
+              </div>
+            </div>
+
+            <div class="vendor  components  flexboxgrid  container-1200
+                        vendor  modificators  flexboxgrid  responsive  container-1800__items--order--1">
+              <div class="vendor  components  flexboxgrid  row">
+                <div class="vendor  components  flexboxgrid  col  col-sm-4">
+
+                </div>
+
+
+                <div class="vendor  components  flexboxgrid  col  col-sm-4">
+
+                </div>
+
+
+                <div class="vendor  components  flexboxgrid  col  col-sm-4">
+
+                </div>
+              </div>
+            </div>
+
+            <div class="vendor  components  flexboxgrid  col  col-xs-2  col-sm-6  col-md-6
+                        vendor  components  flexboxgrid  container-1200--half-lg
+                        vendor  modificators  flexboxgrid  responsive  container-1200--half-lg
+                        vendor  components  flexboxgrid  flex-container
+                        vendor  modificators  flexboxgrid  container--justify-content-flex-end">
+              <?php get_template_part('template-parts/navs/main-nav-1'); ?>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+
+      <div class="mask__mask
+                  vendor  components  flexboxgrid  container-fluid">
+        <div class="vendor  components  flexboxgrid  row
+                    vendor modificators  flexboxgrid  row--full-height">
+          <div class="vendor  components  flexboxgrid  col  col-xs-18
+                      theme  modificators  masks  mask-1"></div>
+        </div>
+      </div>
+
+
+      <div class="mask__image
+                  vendor  components  flexboxgrid  container-fluid">
+        <div class="vendor  components  flexboxgrid  row
+                  vendor modificators  flexboxgrid  row--full-height">
+          <div class="theme  components  images  image-wrapper">
+            <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/bg/bg-11-b&w.jpg'; ?>" alt="alt">
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!--Component mask-->
+
+</header>
+
