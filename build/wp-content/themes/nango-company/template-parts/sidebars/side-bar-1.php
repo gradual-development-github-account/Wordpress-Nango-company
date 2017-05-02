48ef46e8392627d57ea96fee78bc3cce3368410d
Alexandr Ja <mr-spinn@outlook.com>
2017-05-02 21:42:54 +0600

    build/../theme/template-parts/   PHP:      init markup for side-bar-1

---
+++ build/wp-content/themes/nango-company/template-parts/sidebars/side-bar-1.php
@@ -0,0 +1,156 @@
+<aside class="theme  components  sidebars  sidebar-1">
+  <section class="theme  modificators  sections  section-aside-1">
+    <h3 class="theme  modificators  titles  titles-with-line--block-1
+               theme  modificators  fonts  font-family-3  fonts-titles--sm
+               theme  modificators  margins  margin-title--md-2">
+      <span class="text">Category</span>
+      <div class="line
+                  theme  modificators  lines  line-2--block
+                  theme  modificators  gradients  background-gradient-0-deg"></div>
+    </h3>
+    <div class="theme  components  lists  list-1">
+      <div class="item  baseline">
+        <div class="item__text">
+          <a href="#">Renovation</a>
+        </div>
+        <div class="item__subelement"></div>
+        <div class="item__value">( 1 )</div>
+      </div>
+      <div class="item  baseline">
+        <div class="item__text">
+          <a href="#">Renovation</a>
+        </div>
+        <div class="item__subelement"></div>
+        <div class="item__value">( 1 )</div>
+      </div>
+      <div class="item  baseline">
+        <div class="item__text">
+          <a href="#">Renovation</a>
+        </div>
+        <div class="item__subelement"></div>
+        <div class="item__value">( 1 )</div>
+      </div>
+      <div class="item  baseline">
+        <div class="item__text">
+          <a href="#">Renovation</a>
+        </div>
+        <div class="item__subelement"></div>
+        <div class="item__value">( 1 )</div>
+      </div>
+      <div class="item  baseline">
+        <div class="item__text">
+          <a href="#">Renovation</a>
+        </div>
+        <div class="item__subelement"></div>
+        <div class="item__value">( 1 )</div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="theme  modificators  sections  section-aside-1">
+    <h3 class="theme  modificators  titles  titles-with-line--block-1
+               theme  modificators  fonts  font-family-3  fonts-titles--sm
+               theme  modificators  margins  margin-title--md-2">
+      <span class="text">Instagramm</span>
+      <div class="line
+                  theme  modificators  lines  line-2--block
+                  theme  modificators  gradients  background-gradient-0-deg"></div>
+    </h3>
+    <div class="sidebar-1__grid
+                vendor  components  flexboxgrid  row">
+
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_01.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_02.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_03.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_04.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_05.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+      <div class="vendor  components  flexboxgrid  col  col-md-4">
+        <a class="theme  components  images  image-wrapper"  href="#">
+          <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/instagramm-1/inst-3-2-2--slice/inst-3-2-2_06.jpg'; ?>" alt="alt">
+        </a>
+      </div>
+
+    </div>
+  </section>
+
+
+  <section class="theme  modificators  sections  section-aside-1">
+    <h3 class="theme  modificators  titles  titles-with-line--block-1
+               theme  modificators  fonts  font-family-3  fonts-titles--sm
+               theme  modificators  margins  margin-title--md-2">
+      <span class="text">Popular posts</span>
+      <div class="line
+                  theme  modificators  lines  line-2--block
+                  theme  modificators  gradients  background-gradient-0-deg"></div>
+    </h3>
+
+      <div class="theme  components  lists  item-list-1">
+        <div class="vendor  components  flexboxgrid  row
+                    theme  modificators  margins  margin-item--md">
+          <div class="vendor  components  flexboxgrid  col  col-md-4">
+            <a class="theme  components  images  image-wrapper"  href="#">
+              <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/popular-posts-1/popular-posts-1.jpg'; ?>" alt="alt">
+            </a>
+          </div>
+          <div class="vendor  components  flexboxgrid  col  col-md-8">
+            <a href="theme  components  titles
+                     theme  modificators  margins  margin-title--sm">
+              <h4 class="theme  modificators  fonts  font-family-4  fonts-titles--xs">Pellentesque Elit Ullamcorper</h4>
+            </a>
+            <span>Yesterday</span>
+          </div>
+        </div>
+        <div class="vendor  components  flexboxgrid  row
+                    theme  modificators  margins  margin-item--md">
+          <div class="vendor  components  flexboxgrid  col  col-md-4">
+            <a class="theme  components  images  image-wrapper"  href="#">
+              <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/popular-posts-1/popular-posts-1.jpg'; ?>" alt="alt">
+            </a>
+          </div>
+          <div class="vendor  components  flexboxgrid  col  col-md-8">
+            <a href="theme  components  titles
+                     theme  modificators  margins  margin-title--sm">
+              <h4 class="theme  modificators  fonts  font-family-4  fonts-titles--xs">Pellentesque Elit Ullamcorper</h4>
+            </a>
+            <span>Yesterday</span>
+          </div>
+        </div>
+        <div class="vendor  components  flexboxgrid  row
+                    theme  modificators  margins  margin-item--md">
+          <div class="vendor  components  flexboxgrid  col  col-md-4">
+            <a class="theme  components  images  image-wrapper"  href="#">
+              <img class="theme  modificators  images  image--max-width" src="<?php echo get_template_directory_uri() . '/sources/theme/images/widgets/popular-posts-1/popular-posts-1.jpg'; ?>" alt="alt">
+            </a>
+          </div>
+          <div class="vendor  components  flexboxgrid  col  col-md-8">
+            <a href="theme  components  titles
+                     theme  modificators  margins  margin-title--sm">
+              <h4 class="theme  modificators  fonts  font-family-4  fonts-titles--xs">Pellentesque Elit Ullamcorper</h4>
+            </a>
+            <span>Yesterday</span>
+          </div>
+        </div>
+      </div>
+  </section>
+</aside>
