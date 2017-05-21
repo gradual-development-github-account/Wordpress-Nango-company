2baee10166963ce8debefadab546e5e32bdd0a43
Alexandr Ja <mr-spinn@outlook.com>
2017-05-22 00:12:29 +0600

    build/../theme/   PHP:     single: add condition for quality control

--- build/wp-content/themes/nango-company/single.php
+++ build/wp-content/themes/nango-company/single.php
@@ -1,13 +1,14 @@
 <?php
 
-//$post = $wp_query->post;
-
 if        ( $post->post_type === 'blog' ) {
   include ( TEMPLATEPATH . '/template-pages/page-blog-single.php' );
   
 } else if ( $post->post_type === 'portfolio' ) {
   include ( TEMPLATEPATH . '/template-pages/page-portfolio-single.php' );
-  
+
+} else if ( $post->post_type === 'quality-control' ) {
+  include ( TEMPLATEPATH . '/template-pages/page-quality-control-single.php' );
+
 } else {
   //var_dump( get_post_type() );
   echo 'single.php echo';
